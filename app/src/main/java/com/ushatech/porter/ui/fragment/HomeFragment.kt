9fd1c66432df1413af9ac28f9ed85b7708cab1cf
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2023-02-14 16:50:05 +0530

    Update for Access fine location and permissions for google map sdk

--- app/src/main/java/com/ushatech/porter/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/ushatech/porter/ui/fragment/HomeFragment.kt
@@ -1,16 +1,25 @@
 package com.ushatech.porter.ui.fragment
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.location.LocationRequest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.fondesa.kpermissions.allGranted
+import com.fondesa.kpermissions.anyGranted
+import com.fondesa.kpermissions.extension.permissionsBuilder
+import com.fondesa.kpermissions.extension.send
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.*
 import com.ushatech.porter.R
 import com.ushatech.porter.databinding.FragmentHomeBinding
 import com.ushatech.porter.presentation.BaseFragment
@@ -25,7 +34,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : BaseFragment() {
+class HomeFragment : BaseFragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener  {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -55,8 +64,90 @@ class HomeFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        supportMapFragment =
+            (childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment?)!!
+        supportMapFragment!!.getMapAsync(this)
+        checkPermissions()
         return binding.root
 
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = intializeMapData(p0)
+        getLastKnownLocation()
+    }
+    fun checkPermissions() {
+        permissionsBuilder(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+        ).build().send { result ->
+            if (result.anyGranted()) {
+                getLastKnownLocation()
+
+            }else{
+                showToast("Please allow location permission to enable this feature.")
+
+            }
+        }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    fun intializeMapData(mMap: GoogleMap): GoogleMap {
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        mMap.isTrafficEnabled = true
+        //Disabling the map toolbar to get the full interface in the native app only.
+        mMap.uiSettings.isMapToolbarEnabled = false
+        permissionsBuilder(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ).build().send {
+            if (it.allGranted()) {
+                mMap.isMyLocationEnabled = true
+            } else {
+                showToast("Please allow all location permissions")
+
+            }
+
+        }
+
+
+
+
+        return mMap
+
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getLastKnownLocation() {
+        //check for the permission here. !
+        // THE FUSED LOCATION CLIENT OF THE GOOGLE PLAY SERVICES API ARE BETTER
+        // THAN THE android.location (in-built) services.
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location ->
+                if (location != null) {
+
+                    currentLocation = LatLng(location.latitude,location.longitude)
+
+                    var gps = LatLng(location.latitude, location.longitude)
+                    mMap.addMarker(MarkerOptions().position(gps).title("You are here !"))
+                    val cameraPosition = CameraPosition.Builder()
+                        .target(gps) // Sets the center of the map to location user
+                        .zoom(15f) // Sets the zoom
+
+                        .build() // Creates a CameraPosition from the builder
+                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+                    // use your location object
+                    // get latitude , longitude and other info from this
+                    // AFTER GETTING THE CURRENT LOCATION MAKE THE RETROFIT REQUEST. FOR PLACES PREDICTIONS.
+                }
+
+            }
+    }
+    override fun onMarkerClick(p0: Marker): Boolean {
+        showToast("Marker is clicked")
+        return true
     }
 
     companion object {
