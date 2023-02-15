682533ba95edc190521e86ef0a3efbea93a93996
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2023-02-15 12:58:32 +0530

    Add AutoComplete Places from Google Maps Sdk

--- app/src/main/java/com/ushatech/porter/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/ushatech/porter/ui/fragment/HomeFragment.kt
@@ -2,8 +2,11 @@ package com.ushatech.porter.ui.fragment
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.location.LocationRequest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -20,9 +23,16 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.ushatech.porter.R
 import com.ushatech.porter.databinding.FragmentHomeBinding
 import com.ushatech.porter.presentation.BaseFragment
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -50,6 +60,7 @@ class HomeFragment : BaseFragment(), OnMapReadyCallback, GoogleMap.OnMarkerClick
     private lateinit var locationCallback: LocationCallback
     private var currentLocation: LatLng? = null
     private lateinit var supportMapFragment: SupportMapFragment
+    private val AUTOCOMPLETE_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,13 +75,66 @@ class HomeFragment : BaseFragment(), OnMapReadyCallback, GoogleMap.OnMarkerClick
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater)
+        setupGoogleMaps()
+        checkPermissions()
+        setupAutoCompletePlaces()
+        initClicks()
+
+        return binding.root
+
+    }
+
+    private fun setupAutoCompletePlaces() {
+        Places.initialize(fragmentContext, getString(R.string.GOOGLE_API_KEY), Locale.US);
+    }
+
+    private fun setupGoogleMaps() {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
         supportMapFragment =
             (childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment?)!!
-        supportMapFragment!!.getMapAsync(this)
-        checkPermissions()
-        return binding.root
+        supportMapFragment.getMapAsync(this)
+    }
 
+    private fun initClicks() {
+
+        binding.toolbarMaps.btnChangePickLocation.setOnClickListener {
+            // Set the fields to specify which types of place data to
+            // return after the user has made a selection.
+            val fields = listOf(Place.Field.ID, Place.Field.NAME)
+
+            // Start the autocomplete intent.
+            val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields)
+                .setTypesFilter(listOf(TypeFilter.CITIES.toString()))
+                .setCountries(listOf("IN"))
+                .build(fragmentContext)
+            startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE)
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    data?.let {
+                        val place = Autocomplete.getPlaceFromIntent(data)
+                        Log.i("DEV_ASHUTOSH-->", "Place: ${place.name}, ${place.id}")
+                    }
+                }
+                AutocompleteActivity.RESULT_ERROR -> {
+                    // TODO: Handle the error.
+                    data?.let {
+                        val status = Autocomplete.getStatusFromIntent(data)
+                        Log.i("DEV_ASHUTOSH-->", status.statusMessage ?: "")
+                    }
+                }
+                Activity.RESULT_CANCELED -> {
+                    showLog("No location picked by user from Google Auto Suggestions.")
+                }
+            }
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     override fun onMapReady(p0: GoogleMap) {
@@ -151,15 +215,6 @@ class HomeFragment : BaseFragment(), OnMapReadyCallback, GoogleMap.OnMarkerClick
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             HomeFragment().apply {
