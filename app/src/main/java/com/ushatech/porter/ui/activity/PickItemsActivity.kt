ff99fec6ba41b47eaacf68a1f6ec0adc40c30618
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2023-02-16 13:39:36 +0530

    Update for Step 1 and Step 2 manage with bottom navigation as well

--- app/src/main/java/com/ushatech/porter/ui/activity/PickItemsActivity.kt
+++ app/src/main/java/com/ushatech/porter/ui/activity/PickItemsActivity.kt
@@ -7,6 +7,7 @@ import com.ushatech.porter.databinding.ActivityPickItemsBinding
 import com.ushatech.porter.presentation.BaseActivity
 import com.ushatech.porter.ui.fragment.ItemCartFragment
 import com.ushatech.porter.ui.fragment.ItemStepFragment
+import com.ushatech.porter.ui.fragment.SelectLocationFragment
 import com.ushatech.porter.utils.FragmentUtils
 
 class   PickItemsActivity : BaseActivity() {
@@ -30,13 +31,36 @@ class   PickItemsActivity : BaseActivity() {
         if(supportFragmentManager.backStackEntryCount==1){
             finish()
         }
-
         super.onBackPressed()
     }
 
     private fun initClicks() {
         binding.CommonTitleBar.ivClose.setOnClickListener {
             finish()
+        }
+        binding.ivBackFragment.setOnClickListener {
+//            showToast(supportFragmentManager.backStackEntryCount.toString())
+            if(supportFragmentManager.backStackEntryCount==2){
+                binding.tvStepCounter.setText("Steps(1/4)")
+                binding.tvStepTitle.setText("Select items")
+                supportFragmentManager.popBackStack()
+
+            }else if(supportFragmentManager.backStackEntryCount==1){
+                finish()
+            }
+
+        }
+
+        binding.btnProceed.setOnClickListener {
+            // Manage the step 4 fragment navigation here.
+
+
+            if(supportFragmentManager.backStackEntryCount==1){
+                binding.tvStepCounter.setText("Steps(2/4)")
+                binding.tvStepTitle.setText("Location details")
+                FragmentUtils().addFragmentBackStack(supportFragmentManager,R.id.stepsContainer,SelectLocationFragment(),SelectLocationFragment::class.java.simpleName,true)
+            }
+
         }
 
 
